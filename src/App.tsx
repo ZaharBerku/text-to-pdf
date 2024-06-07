cb60c44c385df541aad581e02bbaf0e9d5645039
ZaharBerku <zaharberku@gmail.com>
2024-06-08 00:32:41 +0300

    change title project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { HistoryContextProvider } from "@context/index";
 import { Home } from "@pages/index";
+import { Helmet } from "react-helmet";
 import "./global.css";
 
 function App() {
   return (
-    <HistoryContextProvider>
-      <Home />
-    </HistoryContextProvider>
+    <>
+      <Helmet>
+        <title>Конвертація тексту в PDF</title>
+      </Helmet>
+      <HistoryContextProvider>
+        <Home />
+      </HistoryContextProvider>
+    </>
   );
 }
 
